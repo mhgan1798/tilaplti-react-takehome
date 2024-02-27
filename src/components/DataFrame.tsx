ce50ee05f6a95acb2fe178662194c35a1c179b01
Howard Gan <mhgan1798@gmail.com>
2024-02-27 18:59:22 +0000

    added comments

--- src/components/DataFrame.tsx
+++ src/components/DataFrame.tsx
@@ -1,7 +1,9 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { valueCounts } from "../utils/math";
 
 export function DataFrame(props: { data?: { [key: string]: any }[] }) {
+  const [filteredData, setFilteredData] = useState([]);
+
   // Usememo to load all keys of the data passed in via props
   const columns = useMemo(() => {
     return props.data && Object.keys(props.data[0]);
@@ -16,7 +18,9 @@ export function DataFrame(props: { data?: { [key: string]: any }[] }) {
       {/* Dropdown Filters */}
       <div className="flex flex-col w-1/5 gap-2 p-4 bg-neutral-50 rounded-md h-full">
         <h3>Filters</h3>
+        {/* TODO: I would have turned this into a set of horizontal bars representing a histogram. clicking on one of these bars should filter the data in the table only for those groups selected. The filters would also have to dynamically update based on the data selected. */}
         {/* Segment for each column */}
+        {/* I would have turned valueCounts into a standalone state and update it in a useEffect whenever a bar is clicked. */}
         {columns?.map((col, idx) => {
           return (
             <div className="gap-2" key={idx}>
